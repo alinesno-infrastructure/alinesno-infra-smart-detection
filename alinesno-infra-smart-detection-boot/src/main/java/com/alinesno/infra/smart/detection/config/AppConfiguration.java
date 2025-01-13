610969f672926d8017d5195fed541e8a6fb05fdf
李娜(Allen) <allenAip@gmail.com>
2025-01-13 08:09:26 +0800

    feat:在AppConfiguration类中添加ForestScan注解及相关配置

--- alinesno-infra-smart-detection-boot/src/main/java/com/alinesno/infra/smart/detection/config/AppConfiguration.java
+++ alinesno-infra-smart-detection-boot/src/main/java/com/alinesno/infra/smart/detection/config/AppConfiguration.java
@@ -3,6 +3,8 @@ package com.alinesno.infra.smart.detection.config;
 import com.alinesno.infra.common.facade.enable.EnableActable;
 import com.alinesno.infra.common.web.adapter.sso.enable.EnableInfraSsoApi;
 import com.alinesno.infra.common.web.log.aspect.LogAspect;
+import com.dtflys.forest.springboot.annotation.ForestScan;
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,10 +13,14 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 /**
  * 配置管理
  */
+@Slf4j
 @EnableScheduling
 @EnableActable
 @EnableInfraSsoApi
 @MapperScan("com.alinesno.infra.smart.detection.mapper")
+@ForestScan({
+        "com.alinesno.infra.common.web.adapter.base.consumer"
+})
 @Configuration
 public class AppConfiguration {
 
